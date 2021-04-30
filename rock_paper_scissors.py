3ecdcca1c44904e7cf3ae65b31bc7d30ada63354
sewkani-manya <sewkani.manya564@gmail.com>
2021-04-30 16:38:57 +0530

    Made the computer choose

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,3 +2,6 @@
 import random
 
 user_action=input(" Choose one(rock, paper, scissors:)")
+possible_action=(rock,paper,scissors)
+comp_action=random.randit(possible_action)
+print("you chose", user_action, "the computer chose", comp_action)
